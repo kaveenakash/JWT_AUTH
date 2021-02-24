2de8076cb5ca310289a128171a08952c2eb523e5
kaveenakash <kaveenakashya@gmail.com>
2021-02-24 18:53:56 +0530

    Auth function added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,21 @@ const app = express();
 const jwt = require("jsonwebtoken");
 let refreshTokens = [];
 app.use(express.json());
+ 
+function auth(req, res, next) {
+    let token = req.headers["authorization"];
+    token = token.split(" ")[1]; // Access token
+    
+    jwt.verify(token, "access", (err, user) => {
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+    });
+   }
+   
 
 app.post("/login", (req, res) => {
   const { user } = req.body;
