71f96b5ee80f062e899f7c52499b758969b8726b
kaveenakash <kaveenakashya@gmail.com>
2021-02-24 18:54:48 +0530

    Token renew route added

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,20 @@ const app = express();
 const jwt = require("jsonwebtoken");
 let refreshTokens = [];
 app.use(express.json());
- 
+
 function auth(req, res, next) {
-    let token = req.headers["authorization"];
-    token = token.split(" ")[1]; // Access token
-    
-    jwt.verify(token, "access", (err, user) => {
-      if (!err) {
-        req.user = user;
-        next();
-      } else {
-        return res.status(403).json({ message: "User not authenticated" });
-      }
-    });
-   }
-   
+  let token = req.headers["authorization"];
+  token = token.split(" ")[1]; // Access token
+
+  jwt.verify(token, "access", (err, user) => {
+    if (!err) {
+      req.user = user;
+      next();
+    } else {
+      return res.status(403).json({ message: "User not authenticated" });
+    }
+  });
+}
 
 app.post("/login", (req, res) => {
   const { user } = req.body;
@@ -37,7 +36,30 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/protected", auth, (req, res) => {
-    res.send("Inside protector");
-   });
+  res.send("Inside protector");
+});
+
+app.post("/renewAccessToken", (req, res) => {
+  const refreshToken = req.body.token;
+  if (!refreshToken || !refreshTokens.includes(refreshToken)) {
+    return res.status(403).json({
+      message: "User not authenticated",
+    });
+  }
+  jwt.verify(refreshToken, "refresh", (err, user) => {
+    if (!err) {
+      const accessToken = jwt.sign({ username: user.name }, "access", {
+        expiresIn: "20s",
+      });
+      return res.status(201).json({
+        accessToken,
+      });
+    } else {
+      return res.status(403).json({
+        message: "User not authenticated",
+      });
+    }
+  });
+});
 
 app.listen(3000);
